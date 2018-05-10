8894f46f20d7aca3f93e49b6da902f31c3266f8a
jaugstein <32981259+jaugstein@users.noreply.github.com>
2018-05-10 17:05:59 +0000

    undelete fix

--- undelete2.sh
+++ undelete2.sh
@@ -12,18 +12,20 @@ do
 if [[ $line = "$OLDNAME"* ]]; then
 	# entfernt von links den kürzest möglichen String bis zum Leerzeichen
 	ORIGIN=${line#* }
-
+	
 	# entfernt von links den größt möglichsten String bis /
 	NEWNAME=${line##*/}
 
+	ORIGINPATH=${ORIGIN%/*}
+	echo "Es wird versucht die Datei wieder herzustellen..."
 	# "Wiedeherstellen" der Datei + Überprüfung des Verzeichnis
-	if [[ -d $ORIGIN ]]; then
-		mv $OlDNAME $NEWNAME
+	if [[ -d $ORIGINPATH ]]; then
+		mv $OLDNAME $NEWNAME
 		mv $NEWNAME $ORIGIN
 		# löschen der Zeile in .dir
 		grep -v $line $FILEDIR > $TEMPFILEDIR
 		mv $TEMPFILEDIR $FILEDIR
-	else
+	else 
 		echo "Das ursprüngliche Verzeichnis der Datei existiert nicht"
 	fi
 fi
