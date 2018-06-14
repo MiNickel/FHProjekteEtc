8b9e64f2062e89967a70b21f3544851950967a11
Michael Nickel <michael.nickel@fh-bielefeld.de>
2018-06-14 20:30:23 +0200

    Punkt 1 xd

---
+++ P06.c
@@ -0,0 +1,20 @@
+#inlcude <stdio.h>
+
+typedef struct Job {
+    char *content;
+    char *directory;
+} Student;
+
+char filePath[] = "";
+
+void readThread() {
+
+}
+
+int main() {
+
+    printf("Enter path: \n");
+    scanf("%s", filePath);
+
+    return 0;
+}
