924c3e7f162566cd68d35dee9ee41ee04b2a6e45
Unknown <michael.nickel@fh-bielefeld.de>
2018-06-20 00:22:23 +0200

    lol

---
+++ studboardREST/src/java/test/ParseArtikel.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package REST;
+
+import java.io.Serializable;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Micha2
+ */
+@Path("artikel/parse")
+public class ParseArtikel implements Serializable {
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response artikelParse(String json){
+        System.out.println(json);
+        return Response.status(Response.Status.OK).build();
+    }
+}
